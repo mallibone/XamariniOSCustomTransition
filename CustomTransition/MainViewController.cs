300c0a25738133cf5bac066c3f64beab3fac2e68
Mark Allibone <mallibone@outlook.com>
2018-04-01 07:58:56 +0200

    Initial commit

---
+++ CustomTransition/MainViewController.cs
@@ -0,0 +1,62 @@
+﻿using UIKit;
+using PureLayout.Net;
+using CoreGraphics;
+
+namespace CustomTransition
+{
+    public class MainViewController : UIViewController
+    {
+        UIButton button;
+        public UIButton RoundButton => button;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.White;
+
+            button = new UIButton(UIButtonType.System);
+            button.BackgroundColor = UIColor.Purple;
+            button.SetTitleColor(UIColor.White, UIControlState.Normal);
+            button.SetTitle("Gnabber", UIControlState.Normal);
+            View.AddSubview(button);
+            button.AutoCenterInSuperview();
+
+            var button2 = new UIButton(UIButtonType.System);
+            button2.SetTitle("Go to view predefined", UIControlState.Normal);
+
+            View.AddSubview(button2);
+            button2.BackgroundColor = UIColor.Purple;
+            button2.SetTitleColor(UIColor.White, UIControlState.Normal);
+            button2.AutoAlignAxisToSuperviewAxis(ALAxis.Vertical);
+            button2.AutoPinToBottomLayoutGuideOfViewController(this, 32);
+
+            button.TouchUpInside += (e, s) =>
+            {
+                var vc = new ModalViewController(this)
+                {
+                    ModalPresentationStyle = UIModalPresentationStyle.Custom,
+                    TransitioningDelegate = new GrowTransitioningDelegate(button)
+                };
+
+                NavigationController.PresentViewController(vc, true, null);
+            };
+
+            button2.TouchUpInside += (e, s) =>
+            {
+                var vc = new ModalViewController(this)
+                {
+                    ModalPresentationStyle = UIModalPresentationStyle.Custom,
+                    TransitioningDelegate = new GrowTransitioningDelegate(button2)
+                };
+                NavigationController.PresentViewController(vc, true, null);
+            };
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            button.Frame = new CGRect(button.Frame.X, button.Frame.Y, button.Frame.Width + 8, button.Frame.Height);
+            button.Layer.CornerRadius = 5;
+        }
+    }
+}
---
+++ CustomTransition/ModalViewController.cs
@@ -0,0 +1,43 @@
+﻿using PureLayout.Net;
+using UIKit;
+
+namespace CustomTransition
+{
+    public class ModalViewController : UIViewController
+    {
+        private readonly UIViewController _callee;
+        public ModalViewController(UIViewController callee)
+        {
+            _callee = callee;
+        }
+
+        private UIButton _button;
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View.BackgroundColor = UIColor.Purple;
+
+            _button = new UIButton(UIButtonType.System);
+            _button.SetTitle("Go back", UIControlState.Normal);
+            _button.SetTitleColor(UIColor.White, UIControlState.Normal);
+
+
+            _button.TouchUpInside += (e, s) => _callee.DismissViewController(true, () => { });
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            View.AddSubview(_button);
+            _button.AutoCenterInSuperview();
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            _button.RemoveFromSuperview();
+        }
+    }
+}
